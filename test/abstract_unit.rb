3599db91bd550e2359117ec5d0e9e3204c77328c
ratnikov <ratnikov@gmail.com>
2009-03-19 03:11:18 +0800

    Fixed the database to use sqlite3 for testing
    Signed-off-by: Sofa Design <fabrice.luraine@sofa-design.net>

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -5,11 +5,11 @@ begin
   require 'active_record'
 rescue LoadError
   require 'rubygems'
-  require_gem 'activerecord'
+  require 'active_record'
 end
 
 require File.dirname(__FILE__) + '/../lib/active_record/base_without_table'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
-ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'mysql'])
+ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite3'])
